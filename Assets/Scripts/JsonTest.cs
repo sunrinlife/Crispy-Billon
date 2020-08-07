0db417d70438a80fe83f3319822073fe27427468
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-08-07 14:06:06 +0900

    Created Unity Project

---
+++ Assets/Scripts/JsonTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class JsonTest : MonoBehaviour
+{
+
+	void Start()
+	{
+		StartCoroutine(enumerator());
+	}
+
+	IEnumerator enumerator()
+	{
+		string serverUrl = "https://unitaemin.run.goorm.io/hellfight/swagger.json";
+
+		UnityWebRequest request = UnityWebRequest.Get(serverUrl);
+
+		yield return request.SendWebRequest();
+
+		if (request.isNetworkError)
+		{
+			Debug.Log(request.error);
+		}
+		else
+		{
+			Debug.Log(request.downloadHandler.text);
+		}
+	}
+}
