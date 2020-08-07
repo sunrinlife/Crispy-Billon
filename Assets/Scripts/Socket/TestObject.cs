42f4d55f89c88b2ad54070c4d714ad35c95311d7
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-08-08 06:24:25 +0900

    Revert "Ingame"
    This reverts commit d0caaca0d6e29c6646e1d4238657038e4888b9f2.

---
+++ Assets/Scripts/Socket/TestObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using Quobject.SocketIoClientDotNet.Client;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class TestObject : MonoBehaviour
+{
+	private Socket socket;
+
+	void Start()
+	{
+		socket = IO.Socket("https://sunrinthonsocket.run.goorm.io:80");
+
+		socket.On(Socket.EVENT_CONNECT, () =>
+		{
+			Debug.Log("Connected");
+		});
+
+		socket.On("SendClient", (data) =>
+		{
+			Debug.Log(JSON.Parse(data.ToString())["text"]);
+		});
+	}
+
+	private void FixedUpdate()
+	{
+		var send_data = new Dictionary<string, string>
+		{
+			{"text","test" }
+		};
+
+		socket.Emit("SendServer", JsonConvert.SerializeObject(send_data));
+		Debug.Log("Sended");
+	}
+
+	private void OnDestroy()
+	{
+		socket.Disconnect();
+	}
+}
