25d5133b11e1125ef85fd63d1302e36df4d7d722
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-08-07 19:47:20 +0900

    Making Rhythm Base

---
+++ Assets/Scripts/Rhythm System/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+	public float down_speed = -0.1f;
+
+	private void FixedUpdate()
+	{
+		transform.position += new Vector3(0, down_speed, 0);
+	}
+}
