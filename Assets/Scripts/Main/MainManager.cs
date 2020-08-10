427032698c7cae0f86850a8e00531a7f6cbdb8f6
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-08-10 15:58:32 +0900

    making main

--- Assets/Scripts/Main/MainManager.cs
+++ Assets/Scripts/Main/MainManager.cs
@@ -7,21 +7,13 @@ public class MainManager : MonoBehaviour
 {
 	public GameObject msgbox;
 
-	public GameObject create_input;
-	public GameObject join_input;
-	public GameObject auto_input;
-	public GameObject selectplay;
-	public GameObject selectmult;
-
 	private void Start()
 	{
 		GameObject tmp = Instantiate(msgbox);
 
-		tmp.transform.parent = GameObject.Find("Content").transform;
+		tmp.transform.SetParent(GameObject.Find("Content").transform);
 		tmp.transform.localScale = new Vector3(1, 1, 1);
 		tmp.transform.GetChild(0).GetComponent<Text>().text = "환영합니다!\n혼자서 플레이 하실건가요? \n다른 친구와 함께 플레이 하실건가요 ?";
-
-		selectplay.SetActive(true);
 	}
 
 	public void Single_Fun()
